7aeb887fccad347695c700956260faf26da47958
slavkan777 <info.msdn.com@gmail.com>
2016-09-22 22:27:00 +0300

    New files add

---
+++ GitTest/PatientController.cs
@@ -0,0 +1,362 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using AutoMapper;
+using DoctorPremium.Services.Interfaces;
+using DoctorPremium.DAL.Model;
+using DoctorPremium.Web.Helpers;
+using DoctorPremium.Web.Models;
+using Google.Apis.Drive.v2.Data;
+using log4net;
+using Microsoft.Ajax.Utilities;
+using Microsoft.AspNet.Identity;
+using Org.BouncyCastle.Asn1.Ocsp;
+
+namespace DoctorPremium.Web.Controllers
+{
+	[Authorize(Roles = "Doctor")]
+	public class PatientController : BaseController
+    {
+        private IPatientService _patientService;
+        private IPatientDocumentService _patientDocumentService;
+		private ICountryService _countryService;
+		private ICityService _cityService;
+		private IUserInfoService _UserInfoService;
+		private CountryAndCityHelper _countryAndCityHelper;
+        private IScheduleService _scheduleService;
+
+		public CountryAndCityHelper CountryAndCityHelper
+		{
+			get
+			{
+				return _countryAndCityHelper ?? new CountryAndCityHelper(_countryService, _cityService);
+			}
+		}
+
+		public PatientController(IPatientService patientService, IPatientDocumentService patientDocumentService, ICountryService countryService, ICityService cityService, IUserInfoService userInfoService,IScheduleService scheduleService)
+        {
+            this._patientService = patientService;
+            this._patientDocumentService = patientDocumentService;
+            this._scheduleService = scheduleService;
+			this._countryService = countryService;
+			this._cityService = cityService;
+			this._UserInfoService = userInfoService;
+        }
+
+        public ActionResult Edit(int? id, int? ScheduleRecordID)
+        {
+            PatientEditViewModel model = new PatientEditViewModel();
+
+            if (id == null || id == 0)
+            {
+                model.CreateNewDentalCards();
+	            UserInfo userInfo = _UserInfoService.GetUserInfo(Int32.Parse(User.Identity.GetUserId())); 
+				if (userInfo != null)
+	            {
+		            model.CountryId = userInfo.CountryId;
+		            model.CityId = userInfo.CityId;
+	            }
+            }
+            else
+            {
+                Patient patientInfo = _patientService.GetPatientInfo((int)id, Int32.Parse(User.Identity.GetUserId()));
+                ViewBag.Debt = patientInfo.Debt;
+                if (patientInfo == null)
+                {
+                    throw new HttpException(404, "Patient not found"); // TODO: need not found page
+                }
+                else
+                {
+                    model.FromEntity(patientInfo);
+                }
+            }
+            model.ScheduleRecordId = ScheduleRecordID ?? 0;
+	        FillCountryAndCity(model);
+
+            return View(model);
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Edit(PatientEditViewModel model, CancellationToken cancellationToken)
+        {
+            if (ModelState.IsValid)
+            {
+                string newFileName;
+
+                if (model.PatientId == 0)
+                {
+                    Patient patient = model.ToEntity(model, new Patient());
+                    patient.UserId = Int32.Parse(User.Identity.GetUserId());
+
+                    Patient newPatient = _patientService.Save(patient);
+                    model.PatientId = newPatient.PatientId;
+                    String newPhoto = SavePatientPhoto(model.UserPhoto, model.PatientId, model.Photo);
+                    if (!String.IsNullOrEmpty(newPhoto))
+                    {
+                        model.Photo = newPhoto;
+                    }
+                    
+                    if (model.ScheduleRecordId > 0)
+                    {
+                         ScheduleRecord record = _scheduleService.GetScheduleRecord(Int32.Parse(User.Identity.GetUserId()), model.ScheduleRecordId);
+                        if (record != null)
+                        {
+                            record.PatientId = newPatient.PatientId;
+                            _scheduleService.Save(record);
+                        }
+                    }
+                    _patientService.Save(newPatient);
+                }
+                else
+                {
+
+                    Patient oldPatientInfo = _patientService.GetPatientInfo(model.PatientId, Int32.Parse(User.Identity.GetUserId()));
+                    if (oldPatientInfo == null)
+                    {
+                        throw new HttpException(404, "Patient not found"); // TODO: need not found patient page
+                    }
+                    else
+                    {
+                        Patient patient = model.ToEntity(model, oldPatientInfo);
+                        String newPhoto = SavePatientPhoto(model.UserPhoto, model.PatientId, model.Photo);
+                        if (!String.IsNullOrEmpty(newPhoto))
+                        {
+                            patient.Photo = newPhoto;
+                        }
+                        _patientService.Save(patient);
+                    }
+
+                    //return RedirectToAction("List");
+                }
+
+                return RedirectToAction("Review", new { id = model.PatientId, });
+            }
+			FillCountryAndCity(model);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult UploadDocuments(int patientId, String[] titles)//, IEnumerable<HttpPostedFileBase> files
+        {
+            //var files = Request.Files;
+            List<PatientDocument> documents;
+            if (Request.Files != null && Request.Files.Count > 0)
+            {
+                documents = new List<PatientDocument>();
+                for (int i = 0; i < Request.Files.Count; i++)
+                {
+                    var upFile = Request.Files[i];
+                    if (upFile.ContentLength > 0)
+                    {
+                        String fileName = SavePatientDocument(upFile, patientId);
+                        if (!String.IsNullOrEmpty(fileName))
+                        {
+                            documents.Add(new PatientDocument()
+                            {
+                                PatientId = patientId,
+                                FileName = fileName,
+                                Description = titles[i],
+								CreateDateUtc = DateTime.UtcNow
+                            });
+                        }
+                    }
+                }
+                if (documents.Count > 0)
+                {
+                    _patientDocumentService.SaveList(documents);
+                }
+            }
+            return RedirectToAction("Review", new { id = patientId, });//View("PatientDocumentList");//
+        }
+
+        public ActionResult PatientDocumentList(int patientId)
+        {
+            List<PatientDocument> documents = _patientDocumentService.GetListDocumentsForPatient(patientId);
+            List<PatientDocumentViewModel> models = new List<PatientDocumentViewModel>();
+            if (documents.Count > 0)
+            {
+                Mapper.CreateMap<PatientDocument, PatientDocumentViewModel>();
+                models = Mapper.Map<List<PatientDocument>, List<PatientDocumentViewModel>>(documents);
+            }
+            return PartialView(models);
+        }
+
+        private string SavePatientPhoto(HttpPostedFileBase photo, int patientId, string oldFileName)
+        {
+            String fileName = "";
+            try
+            {
+                if (photo != null && photo.ContentLength > 0 && photo.ContentType.Contains("image"))
+                {
+                    fileName = ImageNameHelper.GetFileNameForPatientPhoto(patientId, photo.FileName);
+                    var path = Path.Combine(Server.MapPath("~/Photos/"),
+                                        System.IO.Path.GetFileName(fileName ?? "image"));
+                    if (!String.IsNullOrEmpty(oldFileName))
+                    {
+                        DeletePatientPhoto(Path.Combine(Server.MapPath("~/Photos/"), System.IO.Path.GetFileName(oldFileName)));
+                    }
+                    photo.SaveAs(path);
+                    return fileName;
+                }
+            }
+            catch (Exception ex)
+            {
+                LogManager.GetLogger(typeof(MvcApplication)).Error(String.Format("Ошибка сохранения файла {0}. patientid={1}", fileName, patientId), ex);
+            }
+
+            return null;
+        }
+
+        private string SavePatientDocument(HttpPostedFileBase file, int patientId)
+        {
+            String fileName = "";
+            try
+            {
+                if (file != null && file.ContentLength > 0)
+                {
+                    fileName = ImageNameHelper.GetFileNameForPatientDoc(patientId, file.FileName);
+                    var path = Path.Combine(Server.MapPath("~/UploadFiles/"),
+                                        System.IO.Path.GetFileName(fileName));
+                    //DeletePatientPhoto(Path.Combine(Server.MapPath("~/Photos/"), System.IO.Path.GetFileName(oldFileName)));
+                    file.SaveAs(path);
+                    return fileName;
+                }
+            }
+            catch (Exception ex)
+            {
+                LogManager.GetLogger(typeof(MvcApplication)).Error(String.Format("Ошибка сохранения файла {0}. patientid={1}", fileName, patientId), ex);
+            }
+
+            return null;
+        }
+
+        private void DeletePatientPhoto(String path)
+        {
+            if (!String.IsNullOrEmpty(path) && System.IO.File.Exists(path))
+            {
+                System.IO.File.Delete(path);
+            }
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ListPatients(DataTableParamModel param)
+        {
+            try
+            {
+
+                string searchString = HttpContext.Request["search[value]"];
+                int recordsTotal = 0;
+                int recordsFiltered = 0;
+                List<Patient> patients = _patientService.GetListToTable(Int32.Parse(User.Identity.GetUserId()), param.Start,
+                    param.Length, searchString, param.Order.ToList()[0].Column, param.Order.ToList()[0].Dir, out recordsTotal, out recordsFiltered);
+                
+                List<PatientListViewModel> items = new List<PatientListViewModel>();
+
+                if (patients.Count > 0)
+                {
+                    Mapper.CreateMap<Patient, PatientListViewModel>()
+                        .ForMember(d => d.FullName,
+                            opt => opt.MapFrom(s => FullNameHelper.GetFullName(s.LastName, s.FirstName, s.SurName))) 
+                            .ForMember(d => d.Phones, opt => opt.MapFrom(s => s.MobilePhone + (s.MobilePhone != null ? ", ":"") + s.HomePhone))
+                        .ForMember(d => d.VisitCount, opt => opt.MapFrom(s => s.PatientVisits.Count))
+                        .ForMember(d => d.IsMale, opt => opt.MapFrom(s => (s.IsMale )))
+                        .ForMember(d => d.LastVisit, opt => opt.MapFrom(s => s.PatientVisits.Count > 0 ? s.PatientVisits.OrderBy(x => x.VisitDate).Last().VisitDate : new DateTime?()));
+                    items = Mapper.Map<List<Patient>, List<PatientListViewModel>>(patients);
+                }
+
+                return Json(new
+                {
+                    draw = param.Draw,
+                    recordsTotal = recordsTotal,
+                    recordsFiltered = recordsFiltered,
+                    data = items
+                },
+                    JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { Result = "ERROR", Message = ex.Message });
+            }
+        }
+
+
+        public ActionResult Review(int? id)
+        {
+            PatientEditViewModel model = new PatientEditViewModel();
+            if (id != null && id != 0)
+            {
+                Patient patientInfo = _patientService.GetPatientInfo((int)id, Int32.Parse(User.Identity.GetUserId()));
+                ViewBag.Debt = patientInfo.Debt;
+                if (patientInfo == null)
+                {
+                    throw new HttpException(404, "Patient not found"); // TODO: need not found patient page
+                }
+                else
+                {
+                    model.FromEntity(patientInfo);
+					FillCountryAndCity(model);
+                }
+            }
+            else
+            {
+                throw new HttpException(404, "Page not found");
+
+            }
+            return View(model);
+        }
+
+
+        [HttpPost]
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult DeleteDataPatient(int id)
+        {
+            bool success = false;
+            if (id != 0)
+            {
+                Patient patientInfo = _patientService.GetPatientInfo((int)id, Int32.Parse(User.Identity.GetUserId()));
+                if (patientInfo == null)
+                {
+                    throw new HttpException(404, "Patient not found"); // TODO: need not found patient page
+                }
+                else
+                {
+                    _patientService.Delete(patientInfo);
+                    success = true;
+                }
+            }
+            else
+            {
+                throw new HttpException(404, "Page not found");
+
+            }
+            var v = new { success = success, error = "" };
+            return Json(v);
+        }
+
+		[NonAction]
+		private void FillCountryAndCity(PatientEditViewModel model)
+		{
+			model.countryItems.AddRange(CountryAndCityHelper.GetCountryDropList());
+			if (model.CountryId != null)
+			{
+				model.cityItems.AddRange(CountryAndCityHelper.GetCityDropList((int)model.CountryId));
+			}
+		}
+    }
+}
---
+++ GitTest/PatientViewModel.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AutoMapper;
+using DoctorPremium.DAL.Model;
+using DoctorPremium.Web.Helpers;
+using System.Net;
+using Microsoft.Ajax.Utilities;
+using Resources;
+
+namespace DoctorPremium.Web.Models
+{
+	public class PatientEditViewModel
+	{
+		public PatientEditViewModel()
+		{
+			genderItems = GenderHelper.GetGenderListItems();
+			countryItems = new List<SelectListItem>();
+			cityItems = new List<SelectListItem>();
+		}
+
+		public Patient ToEntity(PatientEditViewModel model, Patient oldPatient)
+		{
+			Patient newPatient = oldPatient;
+			newPatient.AdditionalInfo = model.AdditionalInfo != null ? model.AdditionalInfo.Trim() : null;
+			newPatient.Address = model.Address != null ? model.Address.Trim() : null;
+			newPatient.AlergoStatus = model.AlergoStatus != null ? model.AlergoStatus.Trim() : null;
+			newPatient.Anamnez = model.Anamnez != null ? model.Anamnez.Trim() : null;
+			newPatient.AnyContact = model.AnyContact != null ? model.AnyContact.Trim() : null;
+			newPatient.CityId = model.CityId;
+			newPatient.Comment = model.Comment != null ? model.Comment.Trim() : null;
+			newPatient.CountryId = model.CountryId;
+			newPatient.Email = model.Email != null ? model.Email.Trim() : null;
+			newPatient.FirstName = model.FirstName != null ? model.FirstName.Trim() : null;
+			newPatient.HomePhone = model.HomePhone != null ? model.HomePhone.Trim() : null;
+			newPatient.ImmunoStatus = model.ImmunoStatus != null ? model.ImmunoStatus.Trim() : null;
+			newPatient.LastName = model.LastName != null ? model.LastName.Trim() : null;
+			newPatient.MobilePhone = model.MobilePhone != null ? model.MobilePhone.Trim() : null;
+			newPatient.SurName = model.SurName != null ? model.SurName.Trim() : null;
+			newPatient.WorkPlace = model.WorkPlace != null ? model.WorkPlace.Trim() : null;
+			newPatient.BirthDate = model.BirthDate ?? new DateTime();
+			newPatient.IsMale = model.IsMale ?? false;
+			newPatient.IsVIP = model.IsVIP;
+			newPatient.Photo = model.Photo;
+			newPatient.CommentDentalCard = model.CommentDentalCard;
+			//Mapper.CreateMap<PatientEditViewModel, Patient>()
+			//	.ForMember(dto => dto.UserId, opt => opt.Ignore())
+			//	.ForMember(dto => dto.PatientId, opt => opt.Ignore())
+			//	.ForMember(dto => dto.CardNumber, opt => opt.Ignore())
+			//	.ForMember(dto => dto.CreateDate, opt => opt.Ignore())
+			//	.ForMember(dto => dto.IsDeleted, opt => opt.Ignore());
+			//newPatient = Mapper.Map(this, newPatient);
+			if (model.DentalCards != null && model.DentalCards.Any())
+			{
+				ICollection<DentalCard> dentalItems = new Collection<DentalCard>();
+				//Mapper.CreateMap<DentalCardModel, DentalCard>()
+				//	.ForMember(dto => dto.PatientId,  opt => opt.UseValue(model.PatientId));
+				//dentalItems = Mapper.Map<IList<DentalCardModel>, ICollection<DentalCard>>(model.DentalCards);
+				foreach (var card in model.DentalCards)
+				{
+					if (!(String.IsNullOrEmpty(card.Description) && !card.IsCheck && card.DentalCardId == 0))
+					{
+						Mapper.CreateMap<DentalCardModel, DentalCard>()
+							.ForMember(dto => dto.PatientId, opt => opt.UseValue(model.PatientId));
+						dentalItems.Add(Mapper.Map<DentalCardModel, DentalCard>(card));
+					}
+				}
+
+				if (newPatient.DentalCards == null || newPatient.DentalCards.Count == 0)
+				{
+					newPatient.DentalCards = dentalItems;
+				}
+				else
+				{
+					DentalCard card;
+					foreach (var item in dentalItems)
+					{
+						card = newPatient.DentalCards.FirstOrDefault(x => x.DentalCardId == item.DentalCardId);
+						if (card != null)
+						{
+							card.Description = item.Description;
+							card.IsCheck = item.IsCheck;
+						}
+						else
+						{
+							newPatient.DentalCards.Add(item);
+						}
+					}
+				}
+			}
+
+			return newPatient;
+		}
+
+		public void FromEntity(Patient patient)
+		{
+			Mapper.CreateMap<Patient,PatientEditViewModel>()
+			.ForMember(dto => dto.DentalCards, opt => opt.Ignore());
+			Mapper.Map(patient, this);
+			if (this.PatientId == 0)
+			{
+				for (int i = 0; i < 32; i++)
+				{
+					this.DentalCards.Add(new DentalCardModel(){OrderNumber = (byte)(i+1)});
+				}
+			}
+			else
+			{
+                if (patient.DentalCards == null || patient.DentalCards.Count == 0)
+                {
+                    CreateNewDentalCards();
+                }
+                else
+                {
+                    Mapper.CreateMap<DentalCard, DentalCardModel>();
+                    this.DentalCards = Mapper.Map<ICollection<DentalCard>, IList<DentalCardModel>>(patient.DentalCards).OrderBy(m => m.OrderNumber).ToList();
+                }
+
+                if (this.DentalCards.Count < 32)
+                {
+                    for (int i = 1; i < 33; i++)
+                    {
+                        if (this.DentalCards.Count < 32 && this.DentalCards.FirstOrDefault(x => x.OrderNumber == i) == null) //i < this.DentalCards.Count 
+                        {
+                            this.DentalCards.Add(new DentalCardModel() { OrderNumber = (byte)i });
+                        }
+                    }
+                    this.DentalCards = this.DentalCards.OrderBy(x => x.OrderNumber).ToList();
+                }
+			}
+		}
+
+		public void CreateNewDentalCards()
+		{
+			this.DentalCards = new List<DentalCardModel>();
+			for (int i = 0; i < 32; i++)
+			{
+				this.DentalCards.Add(new DentalCardModel() { OrderNumber = (byte)(i + 1) });
+			}
+		}
+
+		public int PatientId { get; set; }
+
+        public int UserId { get; set; }
+
+		[Display(Name = "Номер карточки")]
+        public string CardNumber { get; set; }
+
+        //[StringLength(80)]
+        //[Required(ErrorMessageResourceType = typeof(Schedule), ErrorMessageResourceName = "TitleRequired")]
+        //[Display(Name = "Title", ResourceType = typeof(Schedule))]
+
+
+
+
+        [StringLength(64)]
+        [Required(ErrorMessageResourceType = typeof(PatientInfo), ErrorMessageResourceName = "AddLast")]
+        [Display(Name = "Last", ResourceType = typeof(PatientInfo))]
+        public string LastName { get; set; }
+
+
+
+        [StringLength(64)]
+        [Required(ErrorMessageResourceType = typeof(PatientInfo), ErrorMessageResourceName = "AddName")]
+        [Display(Name = "Name", ResourceType = typeof(PatientInfo))]
+         public string FirstName { get; set; }
+
+		[StringLength(64)] 
+        [Display(Name = "Middle", ResourceType = typeof(PatientInfo))]
+        public string SurName { get; set; }
+
+        public string Photo { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(PatientInfo), ErrorMessageResourceName = "AddGender")]
+        [Display(Name = "Gender", ResourceType = typeof(PatientInfo))]
+        public bool? IsMale { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(PatientInfo), ErrorMessageResourceName = "AddBirthDate")]
+        [Display(Name = "BirthDate", ResourceType = typeof(PatientInfo))]
+        public System.DateTime? BirthDate { get; set; }
+
+
+        [Display(Name = "CountryId", ResourceType = typeof(PatientInfo))]
+        public int? CountryId { get; set; }
+
+
+        [Display(Name = "CityId", ResourceType = typeof(PatientInfo))]
+		public int? CityId { get; set; }
+
+
+        //[Required(ErrorMessageResourceType = typeof(PatientInfo), ErrorMessageResourceName = "Address")]
+        
+     
+		[StringLength(200)]
+        [Display(Name = "Address", ResourceType = typeof(PatientInfo))]
+        public string Address { get; set; }
+
+		[StringLength(16)]
+        [Display(Name = "HomePhone", ResourceType = typeof(PatientInfo))] 
+        public string HomePhone { get; set; }
+
+		[StringLength(16)]
+        [Display(Name = "MobilePhone", ResourceType = typeof(PatientInfo))] 
+        public string MobilePhone { get; set; }
+
+		[EmailAddress]
+		[StringLength(80)]
+		[Display(Name = "Email")]
+        public string Email { get; set; }
+
+		[StringLength(200)]
+        [Display(Name = "AnyContact", ResourceType = typeof(PatientInfo))]  
+        public string AnyContact { get; set; }
+
+		[StringLength(200)] 
+        [Display(Name = "WorkPlace", ResourceType = typeof(PatientInfo))]  
+        public string WorkPlace { get; set; }
+
+		[StringLength(4000)]
+        [Display(Name = "Comment", ResourceType = typeof(PatientInfo))]  
+        public string Comment { get; set; }
+
+		[StringLength(500)]
+        [Display(Name = "Anamnez", ResourceType = typeof(PatientInfo))] 
+        public string Anamnez { get; set; }
+
+		[StringLength(500)]
+        [Display(Name = "AlergoStatus", ResourceType = typeof(PatientInfo))] 
+        public string AlergoStatus { get; set; }
+
+		[StringLength(500)]
+        [Display(Name = "ImmunoStatus", ResourceType = typeof(PatientInfo))] 
+        public string ImmunoStatus { get; set; }
+
+		[StringLength(500)]
+        [Display(Name = "AdditionalInfo", ResourceType = typeof(PatientInfo))]
+        public string AdditionalInfo { get; set; }
+
+		[StringLength(400)]
+        [Display(Name = "CommentDentalCard", ResourceType = typeof(PatientInfo))] 
+		public string CommentDentalCard { get; set; }
+
+
+        [Required(ErrorMessageResourceType = typeof(PatientInfo), ErrorMessageResourceName = "IsVIP")]
+        [Display(Name = "IsVIP", ResourceType = typeof(PatientInfo))]
+     	public bool IsVIP { get; set; }
+        public System.DateTime CreateDateUtc { get; set; }
+
+        [Display(Name = "DentalChart", ResourceType = typeof(PatientInfo))]
+        public string Blore { get; set; }
+
+
+        public Nullable<System.DateTime> UpdateDate { get; set; }
+
+
+		public List<SelectListItem> genderItems;
+		public List<SelectListItem> countryItems;
+		public List<SelectListItem> cityItems;
+		public IList<DentalCardModel> DentalCards{ get; set; }
+
+		public HttpPostedFileBase UserPhoto { get; set; }
+
+       [Display(Name = "Balance", ResourceType = typeof(PatientInfo))]
+        public int Debt { get; set; }
+		// public virtual ICollection<PatientVisit> PatientVisits { get; set; }
+
+        public int ScheduleRecordId { get; set; }
+	}
+
+	public class PatientListViewModel
+	{
+		public PatientListViewModel()
+		{
+		}
+
+		public int PatientId { get; set; }
+
+		//public int UserId { get; set; }
+
+		public string CardNumber { get; set; }
+
+		public string FullName { get; set; }
+
+		//public string LastName { get; set; }
+
+		//public string FirstName { get; set; }
+
+		//public string SurName { get; set; }
+
+		public System.DateTime? BirthDate { get; set; }
+
+		//public string HomePhone { get; set; }
+
+		//public string MobilePhone { get; set; }
+
+		public string Phones { get; set; }
+
+		//public string Email { get; set; }
+
+		//public string Comment { get; set; }
+
+		public int VisitCount { get; set; }
+
+		public int Debt { get; set; }
+
+		public bool IsVip { get; set; }
+
+		public DateTime? LastVisit { get; set; }
+        public bool? IsMale { get; set; }
+	}
+
+	public class DentalCardModel
+	{
+		public int DentalCardId { get; set; }
+		public byte OrderNumber { get; set; }
+
+		[StringLength(20)]
+		public string Description { get; set; }
+		public bool IsCheck { get; set; }
+	}
+
+	public class PatientDocumentViewModel
+	{
+		public int PatientDocumentId { get; set; }
+		public int PatientId { get; set; }
+		public string FileName { get; set; }
+		public string Description { get; set; }
+		public System.DateTime CreateDateUtc { get; set; }
+	}
+}
